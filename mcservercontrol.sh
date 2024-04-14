949c849ccb8465a1d3bc4787b95f0f6e98fa62a0
ilightwas <ilightwas@gmail.com>
2024-04-13 21:30:01 -0300

    Fix args for the kill command, let word splitting

--- mcservercontrol.sh
+++ mcservercontrol.sh
@@ -42,7 +42,7 @@ is_server_running() {
 
 cleanup_sleep() {
     sleep_pid=$(/bin/ps | grep sleep | awk "{ print \$1 }")
-    [ -n "$sleep_pid" ] && kill "$sleep_pid"
+    [ -n "$sleep_pid" ] && kill $sleep_pid
 }
 
 try_send_cmd() {
